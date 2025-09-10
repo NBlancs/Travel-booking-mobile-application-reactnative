173744c3a1ec205fef092636f11d101438312ca3
NBlancs <bnoel.omar@gmail.com>
2025-09-10 11:54:07 +0800

    Initial Commit

---
+++ mobile/app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Text>Index</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+})
+
+export default index
