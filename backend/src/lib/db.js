d4c4e118d998779a405cfbe484830b99a2cc91cf
NBlancs <bnoel.omar@gmail.com>
2025-11-15 01:41:45 +0800

    added backend

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import moongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const conn = await moongoose.connect(process.env.MONGO_URI);
+        console.log(`Database connected ${conn.connection.host}`);
+    } catch (error) {
+        console.log("Error connecting to database", error);
+        process.exit(1); // exit with failure
+    }
+
+}
